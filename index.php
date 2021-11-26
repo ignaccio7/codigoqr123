e2145a78697bc1a8571ad5f1ff09fb5fd04ea0de
ignaccio7 <43977105+ignaccio7@users.noreply.github.com>
2021-11-25 21:29:28 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="css/estilos.css">
+</head>
+<body>
+    
+    <div class="login-contenedor">
+        <div class="login-info-contenedor">
+            <h1 class="titulo">Logeo</h1>
+            <form class="formulario-contenedor" action="">
+                <div class="usuario">
+                    <label for="">Usuario :</label><br>
+                    <input type="text" placeholder="Ingresar Usuario"> 
+                </div>
+                <div class="password">
+                    <label for="">Contraseña :</label><br>
+                    <input type="text" placeholder="Ingresar Contraseña">
+                </div> 
+                <button class="boton" type="input">Ingresar</button>               
+            </form>
+        </div>
+        <div class="imagen">
+            <img src="images/login.jpg" alt="">
+        </div>        
+    </div>
+</body>
+</html>
